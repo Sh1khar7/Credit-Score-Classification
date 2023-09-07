6b8bfd86cd7fbb64a0fea195605bf51b823431c2
Shikhar <110851000+Sh1khar7@users.noreply.github.com>
2023-09-07 18:23:13 +0530

    Add files via upload

---
+++ Credit Score Classification (Kaggle) (1).py
@@ -0,0 +1,361 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Importing useful Libraries
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.io as pio
+pio.templates.default = 'plotly_dark'
+
+
+# ## Importing the dataset
+
+# In[2]:
+
+
+file_path="train.csv"
+data_set=pd.read_csv(file_path)
+data_set.info()
+
+
+# ## Checking if the dataset has any NULL values or not
+
+# In[3]:
+
+
+print(data_set.isnull().sum())
+
+
+# ## Credit_Score column values
+
+# In[4]:
+
+
+data_set["Credit_Score"].value_counts()
+
+
+# ## Exploring the occupation feature to know if the occupation of the person affects credit scores
+
+# In[5]:
+
+
+fig = px.box(data_set, 
+             x="Occupation",  
+             color="Credit_Score", 
+             title="Credit Scores Based on the Occupation", 
+             color_discrete_map={'Poor':'red',
+                                 'Standard':'blue',
+                                 'Good':'green'})
+fig.show()
+
+
+# ## Explore whether the Annual Income of the person impacts your credit scores 
+
+# In[6]:
+
+
+fig = px.box(data_set, 
+             x="Credit_Score", 
+             y="Annual_Income", 
+             color="Credit_Score",
+             title="Credit Scores Based on Annual Income", 
+             color_discrete_map={'Poor':'red',
+                                 'Standard':'blue',
+                                 'Good':'green'})
+fig.update_traces(quartilemethod="exclusive")
+fig.show()
+
+
+# ## Explore whether the monthly in-hand salary impacts credit scores
+
+# In[7]:
+
+
+fig = px.box(data_set, 
+             x="Credit_Score", 
+             y="Monthly_Inhand_Salary", 
+             color="Credit_Score",
+             title="Credit Scores Based on Monthly Inhand Salary", 
+             color_discrete_map={'Poor':'red',
+                                 'Standard':'blue',
+                                 'Good':'green'})
+fig.update_traces(quartilemethod="exclusive")
+fig.show()
+
+
+# ## Check if having more bank accounts impacts credit scores
+
+# In[8]:
+
+
+fig = px.box(data_set, 
+             x="Credit_Score", 
+             y="Num_Bank_Accounts", 
+             color="Credit_Score",
+             title="Credit Scores Based on Number of Bank Accounts", 
+             color_discrete_map={'Poor':'red',
+                                 'Standard':'blue',
+                                 'Good':'green'})
+fig.update_traces(quartilemethod="exclusive")
+fig.show()
+
+
+# ##  Impact of the number of credit cards one owns on their credit scores 
+
+# In[9]:
+
+
+fig = px.box(data_set, 
+             x="Credit_Score", 
+             y="Num_Credit_Card", 
+             color="Credit_Score",
+             title="Credit Scores Based on Number of Credit cards", 
+             color_discrete_map={'Poor':'red',
+                                 'Standard':'blue',
+                                 'Good':'green'})
+fig.update_traces(quartilemethod="exclusive")
+fig.show()
+
+
+# ##  Impact of average interest one pays (on loans and EMIs) on their credit scores 
+
+# In[10]:
+
+
+fig = px.box(data_set, 
+             x="Credit_Score", 
+             y="Interest_Rate", 
+             color="Credit_Score",
+             title="Credit Scores Based on the Average Interest rates", 
+             color_discrete_map={'Poor':'red',
+                                 'Standard':'blue',
+                                 'Good':'green'})
+fig.update_traces(quartilemethod="exclusive")
+fig.show()
+
+
+# ## No. of loans one can take at a time for a good credit score
+
+# In[11]:
+
+
+fig = px.box(data_set, 
+             x="Credit_Score", 
+             y="Num_of_Loan", 
+             color="Credit_Score", 
+             title="Credit Scores Based on Number of Loans Taken by the Person",
+             color_discrete_map={'Poor':'red',
+                                 'Standard':'blue',
+                                 'Good':'green'})
+fig.update_traces(quartilemethod="exclusive")
+fig.show()
+
+
+# ## Check  if delaying payments on the due date impacts  credit scores
+
+# In[12]:
+
+
+fig = px.box(data_set, 
+             x="Credit_Score", 
+             y="Delay_from_due_date", 
+             color="Credit_Score",
+             title="Credit Scores Based on Average Number of Days Delayed for Credit card Payments", 
+             color_discrete_map={'Poor':'red',
+                                 'Standard':'blue',
+                                 'Good':'green'})
+fig.update_traces(quartilemethod="exclusive")
+fig.show()
+
+
+# ## Check if frequently delaying payments will impact credit scores
+
+# In[13]:
+
+
+fig = px.box(data_set, 
+             x="Credit_Score", 
+             y="Num_of_Delayed_Payment", 
+             color="Credit_Score", 
+             title="Credit Scores Based on Number of Delayed Payments",
+             color_discrete_map={'Poor':'red',
+                                 'Standard':'blue',
+                                 'Good':'green'})
+fig.update_traces(quartilemethod="exclusive")
+fig.show()
+
+
+# ## Check if having a high credit utilization ratio will affect credit scores
+
+# In[14]:
+
+
+fig = px.box(data_set, 
+             x="Credit_Score", 
+             y="Credit_Utilization_Ratio", 
+             color="Credit_Score",
+             title="Credit Scores Based on Credit Utilization Ratio", 
+             color_discrete_map={'Poor':'red',
+                                 'Standard':'blue',
+                                 'Good':'green'})
+fig.update_traces(quartilemethod="exclusive")
+fig.show()
+
+
+# ## Check if having more debt will affect credit scores
+
+# In[15]:
+
+
+fig = px.box(data_set, 
+             x="Credit_Score", 
+             y="Outstanding_Debt", 
+             color="Credit_Score", 
+             title="Credit Scores Based on Outstanding Debt",
+             color_discrete_map={'Poor':'red',
+                                 'Standard':'blue',
+                                 'Good':'green'})
+fig.update_traces(quartilemethod="exclusive")
+fig.show()
+
+
+# ## Explore how the credit history age of a person affects credit scores
+
+# In[16]:
+
+
+fig = px.box(data_set, 
+             x="Credit_Score", 
+             y="Credit_History_Age", 
+             color="Credit_Score", 
+             title="Credit Scores Based on Credit History Age",
+             color_discrete_map={'Poor':'red',
+                                 'Standard':'blue',
+                                 'Good':'green'})
+fig.update_traces(quartilemethod="exclusive")
+fig.show()
+
+
+# ## Explore how monthly investments affect their credit scores
+
+# In[17]:
+
+
+fig = px.box(data_set, 
+             x="Credit_Score", 
+             y="Amount_invested_monthly", 
+             color="Credit_Score", 
+             title="Credit Scores Based on Amount Invested Monthly",
+             color_discrete_map={'Poor':'red',
+                                 'Standard':'blue',
+                                 'Good':'green'})
+fig.update_traces(quartilemethod="exclusive")
+fig.show()
+
+
+# ## No. of EMIs one can have in a month for a good credit score
+
+# In[18]:
+
+
+fig = px.box(data_set, 
+             x="Credit_Score", 
+             y="Total_EMI_per_month", 
+             color="Credit_Score", 
+             title="Credit Scores Based on Total Number of EMIs per Month",
+             color_discrete_map={'Poor':'red',
+                                 'Standard':'blue',
+                                 'Good':'green'})
+fig.update_traces(quartilemethod="exclusive")
+fig.show()
+
+
+# ## Check if having a low amount at the end of the month affects credit scores
+
+# In[19]:
+
+
+fig = px.box(data_set, 
+             x="Credit_Score", 
+             y="Monthly_Balance", 
+             color="Credit_Score", 
+             title="Credit Scores Based on Monthly Balance Left",
+             color_discrete_map={'Poor':'red',
+                                 'Standard':'blue',
+                                 'Good':'green'})
+fig.update_traces(quartilemethod="exclusive")
+fig.show()
+
+
+# ## Credit Score Classification Model
+
+# In[20]:
+
+
+'''
+As the Credit_Mix column is categorical, 
+so we transform it into a numerical feature 
+so that we can use it to train a Machine Learning model for the task of credit score classification
+'''
+
+data_set["Credit_Mix"] = data_set["Credit_Mix"].map({"Bad": 0,"Standard": 1,"Good": 2})
+
+
+'''
+We split the data into features and labels by selecting the features 
+we found important for our model
+'''
+from sklearn.model_selection import train_test_split
+x = np.array(data_set[["Annual_Income", "Monthly_Inhand_Salary", 
+                   "Num_Bank_Accounts", "Num_Credit_Card", 
+                   "Interest_Rate", "Num_of_Loan", 
+                   "Delay_from_due_date", "Num_of_Delayed_Payment", 
+                   "Credit_Mix", "Outstanding_Debt", 
+                   "Credit_History_Age", "Monthly_Balance"]])
+y = np.array(data_set[["Credit_Score"]])
+
+
+'''
+split the data into training and test sets and 
+proceed further by training a credit score classification model
+'''
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, 
+                                                    test_size=0.33, 
+                                                    random_state=42)
+from sklearn.ensemble import RandomForestClassifier
+model = RandomForestClassifier()
+model.fit(xtrain, ytrain)
+
+
+# In[21]:
+
+
+print("Credit Score Prediction : ")
+a = float(input("Annual Income: "))
+b = float(input("Monthly Inhand Salary: "))
+c = float(input("Number of Bank Accounts: "))
+d = float(input("Number of Credit cards: "))
+e = float(input("Interest rate: "))
+f = float(input("Number of Loans: "))
+g = float(input("Average number of days delayed by the person: "))
+h = float(input("Number of delayed payments: "))
+i = input("Credit Mix (Bad: 0, Standard: 1, Good: 3) : ")
+j = float(input("Outstanding Debt: "))
+k = float(input("Credit History Age: "))
+l = float(input("Monthly Balance: "))
+
+features = np.array([[a, b, c, d, e, f, g, h, i, j, k, l]])
+print("Predicted Credit Score = ", model.predict(features))
+
+
+# In[ ]:
+
+
+
+
